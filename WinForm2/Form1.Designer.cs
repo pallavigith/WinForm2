39c9c11d6b4302f300961b00fe3f842a16e75ba9
padmakar <padmakar@DESKTOP-SQRKHM8>
2022-06-14 12:15:08 +0530

    Add project files.

---
+++ WinForm2/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace WinForm2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmpid = new System.Windows.Forms.Label();
+            this.txtEmpName = new System.Windows.Forms.Label();
+            this.txtSalary = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SHOW = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEmpid
+            // 
+            this.txtEmpid.AutoSize = true;
+            this.txtEmpid.Location = new System.Drawing.Point(212, 36);
+            this.txtEmpid.Name = "txtEmpid";
+            this.txtEmpid.Size = new System.Drawing.Size(39, 13);
+            this.txtEmpid.TabIndex = 0;
+            this.txtEmpid.Text = "Emp id";
+            this.txtEmpid.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.AutoSize = true;
+            this.txtEmpName.Location = new System.Drawing.Point(212, 68);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(59, 13);
+            this.txtEmpName.TabIndex = 1;
+            this.txtEmpName.Text = "Emp Name";
+            this.txtEmpName.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.AutoSize = true;
+            this.txtSalary.Location = new System.Drawing.Point(212, 101);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(36, 13);
+            this.txtSalary.TabIndex = 2;
+            this.txtSalary.Text = "Salary";
+            this.txtSalary.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(330, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(330, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(330, 98);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // SHOW
+            // 
+            this.SHOW.Location = new System.Drawing.Point(230, 221);
+            this.SHOW.Name = "SHOW";
+            this.SHOW.Size = new System.Drawing.Size(75, 23);
+            this.SHOW.TabIndex = 8;
+            this.SHOW.Text = "SHOW";
+            this.SHOW.UseVisualStyleBackColor = true;
+            this.SHOW.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btncancel
+            // 
+            this.btncancel.Location = new System.Drawing.Point(355, 221);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(75, 23);
+            this.btncancel.TabIndex = 9;
+            this.btncancel.Text = "CANCEL";
+            this.btncancel.UseVisualStyleBackColor = true;
+            this.btncancel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.SHOW);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.txtEmpid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtEmpid;
+        private System.Windows.Forms.Label txtEmpName;
+        private System.Windows.Forms.Label txtSalary;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button SHOW;
+        private System.Windows.Forms.Button btncancel;
+    }
+}
+
---
+++ WinForm2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show( " " + txtEmpName.Text + " " + txtSalary.Text+txtEmpid.Text+" ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtSalary.Clear();
+            txtEmpid.Clear();
+            txtEmpName.clear();
+
+
+        }
+    }
+}
